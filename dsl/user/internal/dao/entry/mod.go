7c4b7ea337bd81e92e7b28b3cfc0c1ed3bc3ec23
iegad <iegad@vip.qq.com>
2021-07-12 17:27:55 +0800

    Update mod.go

--- dsl/user/internal/dao/entry/mod.go
+++ dsl/user/internal/dao/entry/mod.go
@@ -2,7 +2,6 @@ package entry
 
 import (
 	"context"
-	"encoding/json"
 
 	"github.com/iegad/kraken/utils"
 	ds "github.com/iegad/mmo/ds/user"
@@ -13,58 +12,37 @@ import (
 func ModEntry(obj *ds.Entry, es *elastic.Client) error {
 	utils.Assert(obj != nil && es != nil, "ModEntry in params is invalid")
 
-	// Step 2: 获取原始数据
-	raw, err := GetEntryByID(obj.UserID, es)
-	if err != nil {
-		return err
-	}
-
-	// Step 3: 验证更新数据
 	updata := map[string]interface{}{}
 
 	// 头像验证
-	if len(obj.Avator) > 0 && obj.Avator != raw.Avator {
+	if len(obj.Avator) > 0 {
 		updata["Avator"] = obj.Avator
 	}
 
 	// 邮箱验证
-	if len(obj.Email) > 0 && obj.Email != raw.Email {
+	if len(obj.Email) > 0 {
 		updata["Email"] = obj.Email
 	}
 
 	// 手机号验证
-	if len(obj.PhoneNum) > 0 && obj.PhoneNum != raw.PhoneNum {
+	if len(obj.PhoneNum) > 0 {
 		updata["PhoneNum"] = obj.PhoneNum
 	}
 
 	// 性别验证
-	if obj.Gender > 0 && obj.Gender != raw.Gender {
+	if obj.Gender > 0 {
 		updata["Gender"] = obj.Gender
 	}
 
 	// 昵称验证
-	if len(obj.Nickname) > 0 && obj.Nickname != raw.Nickname {
+	if len(obj.Nickname) > 0 {
 		updata["Nickname"] = obj.Nickname
-	}
-
-	// 头像验证
-	if len(obj.Avator) > 0 && obj.Avator != raw.Avator {
-		updata["Avator"] = obj.Avator
 	}
 
 	if len(updata) == 0 {
 		return nil
 	}
 
-	up, err := es.Update().Index(dao.N_USER_ENTRY).Id(dao.IDToString(obj.UserID)).Doc(updata).DocAsUpsert(true).FetchSource(true).Do(context.TODO())
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(up.GetResult.Source, obj)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	_, err := es.Update().Index(dao.N_USER_ENTRY).Id(dao.IDToString(obj.UserID)).Doc(updata).DocAsUpsert(true).FetchSource(true).Do(context.TODO())
+	return err
 }
